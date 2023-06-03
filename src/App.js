e017868142ea84165646756cc67bd52f1e77a4db
Jordan Fudge <jordanfudge95@gmail.com>
2023-06-03 19:37:25 -0400

    Added useEffect and additional notes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
-// Below are our components
+import { useState, useEffect } from "react";
+
 import Input from "./Components/Input";
 import Display from "./Components/Display";
 
 function App() {
   const [userData, setUserData] = useState(undefined);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`https://api.github.com/users/Jfudge95`); // Setting our profile page as the initial page when we start-up.
+      const data = await res.json();
+      setUserData(data);
+    };
+
+    fetchData();
+  }, []); //Dependancy Array
 
   const display = userData && <Display userData={userData} />; //Conditional Rendering Display only runs once we get userData.
   return (
@@ -17,6 +27,6 @@ function App() {
 
 export default App;
 
-// conditional rendering
-// username -> what you type -> input
-// user -> data from github -> app
+// Dependancy Array - This is how we control when it runs. Since we want my profile page to display right when we load, we leave that array empty. However, if we put a STATEFUL VARIABLE into the array, it will only run when that Stateful Variable changes.
+// Remember, we are using useEffect here whenever we need to Fetch Data like setting our Profile page as the initial page when the app starts up. By default, useEffect will run on each render.
+// conditional rendering - It allows you to create dynamic user interfaces that can adapt to changes in data and user interactions.
